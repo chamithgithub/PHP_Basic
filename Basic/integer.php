a6facc7b233d97ee47951dfd91af8f38293c0ee0
chamithgithub <dchamith4@gmail.com>
2023-12-09 23:38:08 +0530

    uoppp

---
+++ Basic/integer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>integer_Basic</title>
+</head>
+<body>
+    <?php
+    $num1=30;
+    $num2=56;
+    ?>
+
+    Basic Calculation (((10+20+$num1)*$num2)/20-50):
+    <?php echo(((10+20+$num1)*)*$num2)/20-50);?>
+
+    <h2>Some useful integer functions</h2>
+
+
+
+</body>
+</html>
